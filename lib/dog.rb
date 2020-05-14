060f11584e8a8779b98aeaf1f1e893daa334699b
Ideal-Poster <malcolmgourdine@protonmail.com>
2020-05-13 23:09:40 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+require 'pry'
+
+class Dog
+  @@all = []
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    # binding.pry
+    @@all = []
+  end
+
+  def self.print_all
+    # return Dog.all.map { |dog| dog.name }.join("\n")
+    puts Dog.all.map { |dog| dog.name }.join("\n")
+  end
+
+end
