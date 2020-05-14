34b429532cd9d302891e5d19d474dcd3c5f04fbf
emotionalproof <chaserabenn@gmail.com>
2020-05-14 16:04:07 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,38 +9,33 @@ class Dog
    def initialize(name)
       @name = name
       save
-      binding.pry
    end
 
    def self.all
       @@all
-      binding.pry
    end
 
    def self.print_all
       @@all.each { |dog| puts dog.name }
-      binding.pry
    end
 
    def save
       @@all << self
-      binding.pry
    end
 
    def self.clear_all
       @@all.clear
-      binding.pry
    end
 
 
 end
 
-maizey = Dog.new("Maizey")
-Dog.all
-Dog.print_all
-
-snoopy = Dog.new("Snoopy")
-Dog.all
-Dog.print_all
-
-
+#maizey = Dog.new("Maizey")
+#Dog.all
+#Dog.print_all
+#
+#snoopy = Dog.new("Snoopy")
+#Dog.all
+#Dog.print_all
+#
+#
