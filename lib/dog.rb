d27d138425b3c4d458f8c68097b2897b25472371
emotionalproof <chaserabenn@gmail.com>
2020-05-14 16:02:59 -0400

    update

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,46 @@
-
+#require 'pry'
 
 class Dog
 
-   @@all = []
-
    attr_accessor :name
+
+   @@all = []
 
    def initialize(name)
       @name = name
       save
+      binding.pry
    end
 
-   def Dog.all
+   def self.all
       @@all
+      binding.pry
    end
 
-   def Dog.print_all
-      self.all.each { |dog| puts dog.name }
+   def self.print_all
+      @@all.each { |dog| puts dog.name }
+      binding.pry
    end
 
    def save
       @@all << self
+      binding.pry
    end
 
    def self.clear_all
       @@all.clear
+      binding.pry
    end
 
 
 end
+
+maizey = Dog.new("Maizey")
+Dog.all
+Dog.print_all
+
+snoopy = Dog.new("Snoopy")
+Dog.all
+Dog.print_all
+
+
