182319d6eae6116b6ad5a8dc136958143bdd185c
emotionalproof <chaserabenn@gmail.com>
2020-05-14 08:32:20 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+
+
+class Dog
+
+   @@all = []
+
+   attr_accessor :name
+
+   def initialize(name)
+      @name = name
+      save
+   end
+
+   def Dog.all
+      @@all
+   end
+
+   def Dog.print_all
+      self.all.each { |dog| puts dog.name }
+   end
+
+   def save
+      @@all << self
+   end
+
+   def self.clear_all
+      @@all.clear
+   end
+
+
+end
